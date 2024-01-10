3d88150cef04885c79280bb2967433cd073d8268
jieny <576420147@qq.com>
2024-01-10 14:41:27 +0800

    增加luci-app-mytest

--- package/mypackages/luci-app-mytest/luasrc/model/cbi/xxx/mytest_setup.lua
+++ package/mypackages/luci-app-mytest/luasrc/model/cbi/xxx/mytest_setup.lua
@@ -1,5 +1,13 @@
+-- m = Map("cbi_file", translate("First Tab Form"), translate("Please fill out the form below")) -- cbi_file is the config file in /etc/config
 m = Map("phtunnel")
 m.reset = true
+
+-- d = m:section(TypedSection, "info", "Part A of the form")  -- info is the section called info in cbi_file
+-- a = d:option(Value, "name", "Name"); a.optional=false; a.rmempty = false;  -- name is the option in the cbi_file
+-- 需要一个config文件包含section和options，在这里我们在/etc/confi目录下新建一个cbi_file文件
+-- config 'info' 'A'
+--    option 'name' 'OpenWRT'
+
 
 local s = m:section(NamedSection, "base", "base", translate("Base Setup1"))
 
