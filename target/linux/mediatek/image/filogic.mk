4d6043b5e1e17cfc60782869a088a6e418443311
Chen Minqiang <ptpt52@gmail.com>
2024-06-11 18:29:02 +0800

    mediatek: cmcc_rax3000m: add more apps

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1117,7 +1117,7 @@ define Device/cmcc_rax3000m
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_DTS_LOADADDR := 0x43f00000
   DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 \
-	e2fsprogs f2fsck mkf2fs
+	e2fsprogs f2fsck mkf2fs kmod-fs-ext4 tune2fs ethtool blockd blkid fdisk gdisk partx-utils
   DEVICE_COMPAT_VERSION := 1.1
   DEVICE_COMPAT_MESSAGE := WAN port renamed from eth1 to wan to match its physical \
 	label, check wan interface configuration after upgrade.
